9c4d9b0239f48223a05e625f9c2eef1e904bc9b0
leandromazurek-unemat <leandro.mazurek@unemat.br>
2026-09-24 01:33:40 +0000

    consultando clientes

--- app.py
+++ app.py
@@ -1,4 +1,21 @@
 import streamlit as st 
+import psycopg2
+
+conn = psycopg2.connect(
+    host=st.secrets["DB_HOST"],
+    database=st.secrets["DB_NAME"],
+    user=st.secrets["DB_USER"],
+    password=st.secrets[DB_PASSWORD],
+    port=st.secrets[DB_PORT]
+)
+
+cursor = conn.cursor()
 
 st.title("Sistema de Cadastro") 
 st.write("Bem-vindo ao sistema!")
+
+cursor.execute("SELECT nome, email FROM cliente")
+dados = cursor.fetchall()
+
+for nome, email in dados:
+st.write(f"{nome} - {email}")
