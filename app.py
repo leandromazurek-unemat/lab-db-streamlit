a3324e5d80b15937ffecebba43e79462f1f6b1f2
leandromazurek-unemat <leandro.mazurek@unemat.br>
2026-09-24 01:38:02 +0000

    consultando clientes corrido denovo

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ conn = psycopg2.connect(
     host=st.secrets["DB_HOST"],
     database=st.secrets["DB_NAME"],
     user=st.secrets["DB_USER"],
-    password=st.secrets[DB_PASSWORD],
-    port=st.secrets[DB_PORT]
+    password=st.secrets["DB_PASSWORD"],
+    port=st.secrets["DB_PORT"]
 )
 
 cursor = conn.cursor()
